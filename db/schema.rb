80c8c0ce80c07d78efdfc6aba91b14841769f5f4
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-07-15 01:42:38 +0800

    added comments on links show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714113303) do
+ActiveRecord::Schema.define(version: 20150714171151) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +33,17 @@ ActiveRecord::Schema.define(version: 20150714113303) do
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "main_image_file_name"
+    t.string   "main_image_content_type"
+    t.integer  "main_image_file_size"
+    t.datetime "main_image_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
